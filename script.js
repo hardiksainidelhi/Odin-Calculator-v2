99441023e5182c42a9351617b088c37c23ab16db
hardiksainidelhi <hardiksainidelhi@gmail.com>
2025-05-15 17:48:37 +0000

    Add basic functionality of calculator

--- script.js
+++ script.js
@@ -2,31 +2,117 @@
 let numberA
 let numberB
 let operation
+let operatorButtonActive
 
-function add(a,b){
+function add(a, b) {
     return +a + +b
 }
 
-function subtract(a,b){
+function subtract(a, b) {
     return +a - +b
 }
 
-function multiply(a,b){
-    return a*b
+function multiply(a, b) {
+    return a * b
 }
-function divide(a,b){
+function divide(a, b) {
     return +a / +b
 }
 
-function operate(numberA,numberB,operation){
+function operate(numberA, numberB, operation) {
+    let result;
     switch (operation) {
         case '+':
-            return add(numberA,numberB)
+            result = add(numberA, numberB)
+            break
         case '-':
-            return subtract(numberA,numberB)
+            result = subtract(numberA, numberB)
+            break
         case '*':
-            return multiply(numberA,numberB)
+            result = multiply(numberA, numberB)
+            break
         case '/':
-            return divide(numberA,numberB)
+            result = divide(numberA, numberB)
+            break
+    }
+    return Math.round(result * Math.pow(10, 5)) / Math.pow(10, 5)
+}
+
+function updateDisplay(event) {
+    if (display.innerText === '0' || operatorButtonActive) {
+        display.innerText = ''
+        operatorButtonActive = false
+    }
+    display.innerText += event.target.innerText
+}
+
+function setDisplay(num) {
+    display.innerText = num
+}
+
+function getDisplayNumber() {
+    return +display.innerText
+}
+
+function clearDisplay() {
+    display.innerText = '0'
+}
+
+function allClear() {
+    operation = null
+    numberA = null
+    numberB = null
+}
+
+function setNumberAndOperator(event) {
+    numberA = getDisplayNumber()
+    operation = event.target.innerText
+    clearDisplay()
+}
+
+function compute(event) {
+    numberB = getDisplayNumber()
+    console.log(numberB, numberA, operation)
+    if (!(numberB == null || numberA == null || operation == null)) {
+        const result = operate(numberA, numberB, operation)
+        setDisplay(result)
+        allClear()
+        operatorButtonActive = true
     }
 }
+
+const display = document.querySelector('#calc-display')
+const numButtons = document.querySelectorAll('.num-button')
+const operationButtons = document.querySelectorAll('.operation-button')
+const operateButton = document.querySelector('#operate')
+const clearButton = document.querySelector('#C')
+const allClearButton = document.querySelector('#AC')
+
+clearButton.addEventListener('click', event => clearDisplay())
+allClearButton.addEventListener('click', event => {
+    allClear()
+    clearDisplay()
+})
+
+numButtons.forEach(btn => {
+    btn.addEventListener('click', updateDisplay)
+})
+
+operationButtons.forEach(btn => {
+    btn.addEventListener('click', event => {
+        if (!(operation)) setNumberAndOperator(event)
+        else {
+            if (!(operatorButtonActive)) {
+                numberB = getDisplayNumber()
+                const result = operate(numberA, numberB, operation)
+                console.log(`${numberA} ${operation} ${numberB} = ${result}`)
+                numberA = result
+                setDisplay(numberA)
+                operatorButtonActive = true
+            }
+            operation = event.target.innerText
+        }
+    })
+})
+
+operateButton.addEventListener('click', compute)
